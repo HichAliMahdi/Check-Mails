d7d8ca6687071b52b6a2d2f689dffa745202fceb
HichAliMahdi <59925986+HichAliMahdi@users.noreply.github.com>
2020-05-08 18:36:16 +0200

    Update and rename sc.py to script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 from validate_email import validate_email
-with open('sa.txt','r') as f:
+with open('email_file','r') as f:
     email_list = f.readlines()
 print("number of emails in the list : ",len(email_list))
 valide_emails = []
 
 for i,email in enumerate(email_list) :
-    print("\rtraitement de l'emai numero : {}-{}".format(i,email))
+    print("\rtraitement de l'email numero : {}-{}".format(i,email))
     if validate_email(email.strip(),verify=True):
          valide_emails.append(email.strip())
 print("number of valide emails : ",len(valide_emails))
